fb9173b3d6ff673cd7caf7b46e5f4798dbca1fb2
Matthew BEAUDOUIN-LAFON <mattbl@Matthews-MacBook-Pro.local>
2018-12-24 14:37:26 +0100

    Lucas-Kanade flow works

--- src/main.py
+++ src/main.py
@@ -84,6 +84,36 @@ def lk_optical_flow(video_path):
     cv.destroyAllWindows()
     cap.release()
 
+def dense_optical_flow(video_path):
+    """
+    Calculates dense optical flow on video (ie does it on every pixel).
+    Code taken from
+        https://docs.opencv.org/3.4/d7/d8b/tutorial_py_lucas_kanade.html
+    """
+    cap = cv.VideoCapture(video_path)
+    ret, frame1 = cap.read()
+    prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
+    hsv = np.zeros_like(frame1)
+    hsv[...,1] = 255
+    while(1):
+        ret, frame2 = cap.read()
+        next = cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
+        flow = cv.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
+        hsv[...,0] = ang*180/np.pi/2
+        hsv[...,2] = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
+        bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
+        cv.imshow('frame2',bgr)
+        k = cv.waitKey(30) & 0xff
+        if k == 27:
+            break
+        elif k == ord('s'):
+            cv.imwrite('opticalfb.png',frame2)
+            cv.imwrite('opticalhsv.png',bgr)
+        prvs = next
+    cap.release()
+    cv.destroyAllWindows()
+
 if __name__ == "__main__":
-    lk_optical_flow(VIDEO_PATH)
-    # dense_optical_flow(VIDEO_PATH)
+    # lk_optical_flow(VIDEO_PATH)
+    dense_optical_flow(VIDEO_PATH)
